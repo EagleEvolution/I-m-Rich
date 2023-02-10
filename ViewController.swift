3280a5431b75a824d3f895d94ff332fa9ca2b07d
Shahbaz khan <72485834+EagleEvolution@users.noreply.github.com>
2023-02-10 12:09:32 +0530

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm Rich
+//
+//  Created by Shahbaz Khan on 10/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
